b5d2797dbf9b2b3c66799dd6c145b33933ca007b
mattia <mattia.asti@gmail.com>
2019-01-24 21:55:36 +0000

    Added build

---
+++ casa-galgagnano/build/precache-manifest.0f4e2109e0c072144edfd6375207b789.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2149fee0334e51e4ea62",
+    "url": "/static/js/main.2149fee0.chunk.js"
+  },
+  {
+    "revision": "29a1eefbe31698624f7a",
+    "url": "/static/js/1.29a1eefb.chunk.js"
+  },
+  {
+    "revision": "402cd425512d4414bff012020f841019",
+    "url": "/index.html"
+  }
+];
